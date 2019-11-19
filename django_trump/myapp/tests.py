af9eb109f6a831be1dd1dc75020aeaed4daeaf13
o-Cytokine-o <b7072@gmail.com>
2019-11-19 13:50:38 +0900

    アシスト関数実装

--- django_trump/myapp/tests.py
+++ django_trump/myapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+import numpy as np
+import assist_text  as assist
 
-# Create your tests here.
+# Create your tests here
+hand_arr = np.array([[9],[4,6,8],[1,10],[6,10,7],[8,8]])
+
+print(assist.get_assist(hand_arr))
