e6ff776390b91b1ba06028aff2a6f59138fd2683
kojima-k <gukkru6@gmail.com>
2019-11-19 14:41:49 +0900

    チュートリアル編集

--- django_trump/myapp/tokui.py
+++ django_trump/myapp/tokui.py
@@ -1,40 +1,41 @@
-def sakaguti():
+# sakaguti関数とは、
 
+def sakaguti(state_frag):
 
-    frag = 0
-    hoge1 = 1
-    hoge2 = 0
-    hoge3 = 0
-    hoge4 = 0
-    hoge5 = 0
     text=[]
-    d="ディーラーは各プレイヤーにカード配布してください。"
-    p1="プレイヤー1はアシスト画面に従い自身の行動を選択してください。"
-    p2="プレイヤー1は終了する場合は終了を宣言してください。"
-    p3="プレイヤー2はアシスト画面に従い自身の行動を選択してください。"
-    p4="プレイヤー2は終了する場合は終了を宣言してください。"
-    p5="プレイヤー3はアシスト画面に従い自身の行動を選択してください。"
-    p6="プレイヤー3は終了する場合は終了を宣言してください。"
-    p7="プレイヤー4はアシスト画面に従い自身の行動を選択してください。"
-    p8="プレイヤー4は終了する場合は終了を宣言してください。"
+    d="ディーラーが全プレイヤーにカード配布するまでお待ちください。"
+    p="それでは深い闇のゲームを開始します。"
+    p1="プレイヤー1はアシスト画面に従い自身の行動を選択し宣言してください。"
+    p2="プレイヤー1は自身の行動を終える場合、ターンエンドを宣言してください。"
+    p3="プレイヤー2はアシスト画面に従い自身の行動を選択し宣言してください。"
+    p4="プレイヤー2は自身の行動を終える場合、ターンエンドを宣言してください。"
+    p5="プレイヤー3はアシスト画面に従い自身の行動を選択し宣言してください。"
+    p6="プレイヤー3は自身の行動を終える場合、ターンエンドを宣言してください。"
+    p7="プレイヤー4はアシスト画面に従い自身の行動を選択し宣言してください。"
+    p8="プレイヤー4は自身の行動を終える場合、ターンエンドを宣言してください。"
+    p9="ディーラーのカードの合計が、17以上になるまでカードを引き、その合計値で勝敗が決まります。"
 
-    if frag==hoge1:
+    if state_frag==0:
         text.append(d)
-    elif frag==hoge2:
+    elif state_frag==1:
+        text.append(p)
         text.append(p1)
-        text.append(p2)
-    elif frag==hoge3:
+        text.append(p2)    
+    elif state_frag==2:
         text.append(p3)
         text.append(p4)
-    elif frag==hoge4:
+    elif state_frag==3:
         text.append(p5)
         text.append(p6)
-    elif frag==hoge5:
+    elif state_frag==4:
         text.append(p7)
         text.append(p8)
+    elif state_frag==5:
+        text.append(p9)
+    elif state_frag==6:
+        text.append()
     else:
         text.append("unko")
-
 
     return text
 
