bc0744eba7c2efaeb5ec6f64d18f07aa1bd155b9
Umemoto0608 <b7046@oic.jp>
2019-11-22 13:48:52 +0900

    gamegamen

--- django_trump/myapp/wsserver.py
+++ django_trump/myapp/wsserver.py
@@ -4,7 +4,7 @@ def new_client(client, server):
     server.send_message_to_all("Hey all, a new client has joined us")
 
 def send_msg_allclient(client, server,message):
-    server.send_message_to_all("Hey all:"+message)
+    server.send_message_to_all(message)
 
 server = WebsocketServer(9990, host='localhost')
 server.set_fn_new_client(new_client)
