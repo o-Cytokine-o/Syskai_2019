682baabf6b0abd1f9a6f35e00029327202f3a100
o-Cytokine-o <b7072@gmail.com>
2019-11-26 21:13:05 +0900

    文字化け修正、ゲーム画面レイアウト改善

--- django_trump/myapp/client.py
+++ django_trump/myapp/client.py
@@ -3,4 +3,13 @@ from websocket import create_connection
 
 def send_data(ws,data1,data2):
     print('クライアントデバッグ：'+str(data1)+':'+str(data2))
-    ws.send(str(data1)+':'+str(data2))
+    #dataに入っている文字列を全て組み合わせる
+    text1 = ''
+    text2 = ''
+    for st in data1:
+        text1 = text1 + ',' + st
+    text1 = text1[1:]
+    for st in data2:
+        text2 = text2 + ',' + st
+    text2 = text2[1:]
+    ws.send(str(text1)+':'+str(text2))
--- django_trump/myapp/wsserver.py
+++ django_trump/myapp/wsserver.py
@@ -1,12 +1,12 @@
 from websocket_server import WebsocketServer
 
 def new_client(client, server):
-    server.send_message_to_all("Hey all, a new client has joined us")
+    server.send_message_to_all("")
 
 def send_msg_allclient(client, server,message):
     server.send_message_to_all(message)
 
 server = WebsocketServer(9990, host='localhost')
-server.set_fn_new_client(new_client)
+
 server.set_fn_message_received(send_msg_allclient)
 server.run_forever()
